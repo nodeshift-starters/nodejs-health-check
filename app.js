4bd85b81b24d9b784eaf4736bc91143d5dac53ab
Lucas Holmquist <lholmqui@redhat.com>
2017-07-28 15:01:31 -0400

    basic health check functioning

---
+++ app.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+let isOnline = true;
+
+//
+app.use('/api/greeting', (request, response) => {
+  if (!isOnline) {
+    return response.sendStatus(400);
+  }
+
+  const name = request.query ? request.query.name : undefined;
+  return response.send({content: `Hello, ${name || 'World'}`});
+});
+
+app.use('/api/killme', (request, response) => {
+  isOnline = false;
+  return response.send('Stopping HTTP server, Bye bye world !');
+});
+
+app.use('/api/health/readiness', (request, response) => {
+  return response.send('OK');
+});
+
+app.use('/api/health/liveness', (request, response) => {
+  return isOnline ? response.send('OK') : response.sendStatus(500);
+});
+
+module.exports = app;
